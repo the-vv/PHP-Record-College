7061a7e994cb57fb52a7f2cb409f7f0c819ca02a
vv-2001 <vishnuvinod2772001@gmail.com>
2021-02-23 12:12:20 +0530

    added upto input forms get

---
+++ 1/4.php
@@ -0,0 +1,29 @@
+<?php
+$week = 5;
+switch($week)
+{
+case 1:
+echo "Sunday";
+break;
+case 2:
+echo "Monday";
+break;
+case 3:
+echo "Tuesday";
+break;
+case 4:
+echo "Wednessday";
+break;
+case 5:
+echo "Thursday";
+break;
+case 6:
+echo "Friday";
+break;
+case 7:
+echo "Saturday";
+break;
+default:
+echo "Not a week day";
+}
+?>
