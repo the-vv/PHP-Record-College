90adbccbb9ed050e2f8b3c99faa7a159f5602f5b
vv-2001 <vishnuvinod2772001@gmail.com>
2021-04-11 20:40:34 +0530

    added update program

---
+++ 17-db-updation.php
@@ -0,0 +1,75 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$conn = mysqli_Connect($servername, $username, $password);
+if (!$conn) {
+    echo "<br>Connection failed" . mysqli_connect_error();
+}
+$sql = "CREATE DATABASE IF NOT EXISTS classroom";
+if (mysqli_query($conn, $sql)) {
+    echo "<br>database Created successfully <br>";
+} else {
+    echo "<br>error creating database" . mysqli_error($conn);
+}
+mysqli_select_db($conn, "classroom");
+$sql = "CREATE TABLE IF NOT EXISTS s4bca (id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(10) NOT NULL, email VARCHAR(15) NOT NULL, mobile INT(15) NOT NULL)";
+if (mysqli_query($conn, $sql)) {
+    echo "<br>Table created successfully<br>";
+} else {
+    echo "<br>error creating table " . mysqli_error($conn);
+}
+?>
+<html>
+
+<body>
+    <form method="post">
+        <h2>
+            Delete Data From DB
+        </h2>
+        Enter ID to update
+        <input type="text" name="uid">
+        <br>   
+        Select which value to update     
+        <select name="col" id="">
+        <option value="name">Name</option>
+        <option value="email">Email</option>
+        <option value="mobile">Mobile</option>
+        </select>
+        <br>        
+        Enter the value
+        <input type="text" name="value">
+        <br>
+        <input type="submit" value="update" name="delete">
+        <br><br><br>
+</body>
+
+</html>
+<?php
+function show()
+{
+    echo "<table border='1'><tr><th>ID</th><th>NAME</th><th>EMAIL</th><th>MOBILE</th></tr>";
+    $sql = "SELECT * FROM s4bca";
+    global $conn;
+    $rs = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($rs)) {
+        echo "<tr><td>" . $row['id'] . "</td><td>" . $row['name'] . "</td><td>" . $row['email'] . "</td><td>" . $row['mobile'] . "</td></td>";
+    }
+}
+if (isset($_POST['delete'])) {
+    $id = $_POST['uid'];
+    if($_POST['col'] == 'name') {
+        $sql = "UPDATE s4bca SET name='{$_POST['value']}' WHERE id=$id";
+    } elseif($_POST['col'] == 'email') {
+        $sql = "UPDATE s4bca SET email='{$_POST['value']}' WHERE id=$id";
+    } elseif($_POST['col'] == 'mobile') {
+        $sql = "UPDATE s4bca SET mobile='{$_POST['value']}' WHERE id=$id";
+    }
+    if (mysqli_query($conn, $sql)) {
+        echo "<br>Record of ID: " . $id . " Successfully updated";
+        show();
+    } else {
+        echo "<br> Error in Deletion " . mysqli_error($conn);
+    }
+}
+?>
